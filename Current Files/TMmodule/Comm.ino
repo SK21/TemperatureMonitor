e7267c7454b299ba0477f5e74283719d47ce1770
SK21 <SK21@users.noreply.github.com>
2023-08-11 14:25:41 -0600

    update folders

---
+++ Current Files/TMmodule/Comm.ino
@@ -0,0 +1,231 @@
+
+void ConnectWifi()
+{
+	Serial.println("");
+	Serial.print("Connecting to ");
+	Serial.println(MDL.SSID);
+	WiFi.mode(WIFI_AP_STA);
+	WiFi.begin(MDL.SSID, MDL.Password);
+	ErrorCount = 0;
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(1000);
+		Serial.print(".");
+		if (ErrorCount++ > 20) break;
+	}
+
+	if (WiFi.status() == WL_CONNECTED)
+	{
+		Serial.println("");
+		Serial.println("WiFi connected");
+		Serial.println("IP address: ");
+		Serial.println(WiFi.localIP());
+		Serial.println("");
+
+		WiFi.setAutoReconnect(true);
+		WiFi.persistent(true);
+	}
+	else
+	{
+		Serial.println("");
+		Serial.println("WiFi not connected");
+		Serial.println("");
+	}
+}
+
+void CheckTempServer()
+{
+
+	ServerConnected = pulsewait > (millis() - pulsetime);
+	if (!ServerConnected)
+	{
+		// check if time between connection attempts is greater than setting
+		if (millis() - LastServerConnectTime > ServerConnectInterval)
+		{
+			LastServerConnectTime = millis();
+			ErrorCount = 0;
+
+			Serial.print("Connecting to server at ");
+			Serial.print(IPtoString(MDL.ServerIP));
+			Serial.println(" ...");
+
+			while (!ServerConnected)
+			{
+				ServerConnected = client.connect(MDL.ServerIP, MDL.Port);
+				delay(1000);
+				Serial.print(".");
+				if (ErrorCount++ > 5) break;
+			}
+
+			if (ServerConnected)
+			{
+				pulsetime = millis();
+				Serial.println("");
+				Serial.println("Server connected.");
+			}
+			else
+			{
+				Serial.println("");
+				Serial.println("Server not connected.");
+			}
+		}
+	}
+}
+
+void ReadTempServer()
+{
+	int Count = 0;
+	while (client.available())
+	{
+		char c = client.read();
+		Count = Count + 1;
+		if (Count > maxread) // max characters
+		{
+			return;
+		}
+		else
+		{
+			packetin = packetin + c;
+		}
+	}
+}
+
+void checkchunks()
+{
+	while (getchunks())  //break message up into parts, keep getting chunks from packet
+	{
+		processchunks();  //take action based on message
+		delay(100);
+	}
+	packetin = "";  //erase any junk
+}
+
+boolean getchunks()
+{
+	int St = packetin.indexOf("^");
+	if (St == -1)
+	{
+		// ignore message, no start character
+		return false;
+	}
+	int D1 = packetin.indexOf("|", St + 1);
+	if (D1 == -1)
+	{
+		return false;
+	}
+	int D2 = packetin.indexOf("|", D1 + 1);
+	if (D2 == -1)
+	{
+		// no second delimiter
+		return false;
+	}
+	int D3 = packetin.indexOf("|", D2 + 1);
+	if (D3 == -1)
+	{
+		// no third delimiter
+		return false;
+	}
+	P1 = packetin.substring(St + 1, D1).toInt();
+	P2 = packetin.substring(D1 + 1, D2);
+	P3 = packetin.substring(D2 + 1, D3);
+
+	packetin.remove(0, D3);  // remove processed part
+	return true;
+}
+
+float Tmp;
+void processchunks()
+{
+	switch (P1)
+	{
+	case 0:
+		// heartbeat, reset time
+		pulsetime = millis();
+		break;
+
+	case 1:
+		// get temperatures
+		if (DS2842Connected)
+		{
+			UpdateSensorsMaster();
+		}
+		else
+		{
+			UpdateSensors();
+		}
+
+		if (P3 == "")
+		{
+			// all sensors
+			for (int i = 0; i < SensorCount; i++)
+			{
+				Tmp = (float)((int16_t)(Sensors[i].Temperature[1] << 8 | Sensors[i].Temperature[0]) / 16.0);
+				packetout = String(Tmp);
+				String Rom = addressTostring(Sensors[i].ID);
+				sendpacket(1, packetout, Rom);
+			}
+		}
+		else
+		{
+			// single sensor
+			for (int i = 0; i < SensorCount; i++)
+			{
+				String Rom = addressTostring(Sensors[i].ID);
+				if (P3 == Rom)
+				{
+					Tmp = (float)((int16_t)(Sensors[i].Temperature[1] << 8 | Sensors[i].Temperature[0]) / 16.0);
+					packetout = String(Tmp);
+					sendpacket(1, packetout, Rom);
+				}
+			}
+		}
+		break;
+
+	case 2:
+		//set user data
+		for (int i = 0; i < SensorCount; i++)
+		{
+			String Rom = addressTostring(Sensors[i].ID);
+			Rom.trim();
+			P3.trim();
+			if (P3.equals(Rom))
+			{
+				int userdata = P2.toInt();
+				if (DS2842Connected)
+				{
+					SetUserDataMaster(Sensors[i].ID, userdata);
+				}
+				else
+				{
+					SetUserData(Sensors[i].ID, Sensors[i].BusID, userdata);
+				}
+				break;
+			}
+		}
+		break;
+
+	case 3:
+		//get user data
+		for (int i = 0; i < SensorCount; i++)
+		{
+			String Rom = addressTostring(Sensors[i].ID);
+			Rom.trim();
+			P3.trim();
+			if (P3 == Rom)
+			{
+				uint16_t UserData = Sensors[i].UserData[1] << 8 | Sensors[i].UserData[0];
+				packetout = String(UserData);
+				sendpacket(3, packetout, Rom);
+				break;
+			}
+		}
+		break;
+	}
+}
+
+void sendpacket(int id, String data, String Rom)
+{
+	data = "^" + String(id) + "|" + data + "|" + Rom + "|";
+	client.print(data);
+}
+
---
+++ Old Files/TMbasic2/SensorsMaster.ino
@@ -0,0 +1,86 @@
+
+// adapted from https://playground.arduino.cc/Learning/OneWire/
+
+void UpdateSensorsMaster()
+{
+	byte Addr[8];
+	byte Mem[2];
+	SensorCount = 0;
+
+	Serial.println();
+	Serial.println("Searching one-wire bus... ");
+	OneWireMaster.wireResetSearch();
+	delay(500);
+	while (OneWireMaster.wireSearch(Addr) && SensorCount < 256)
+	{
+		if (GetTempMaster(Addr, Mem))
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				Sensors[SensorCount].ID[i] = Addr[i];
+			}
+			Sensors[SensorCount].Temperature[0] = Mem[0];
+			Sensors[SensorCount].Temperature[1] = Mem[1];
+
+			Sensors[SensorCount].UserData[1] = (byte)(UserData >> 8);
+			Sensors[SensorCount].UserData[0] = (byte)(UserData);
+			SensorCount++;
+		}
+		delay(500);
+	}
+	Serial.println("Sensor count: " + String(SensorCount));
+}
+
+bool GetTempMaster(byte Addr[], byte Mem[])
+{
+	float Result = -127.0;
+	UserData = 0;
+	byte dsScratchPadMem[9];
+
+	for (int i = 0; i < 3; i++)
+	{
+		OneWireMaster.wireReset();
+		OneWireMaster.wireSelect(Addr);
+		OneWireMaster.wireWriteByte(0x44);  
+
+		delay(1000);
+		OneWireMaster.wireReset();
+		OneWireMaster.wireSelect(Addr);
+		OneWireMaster.wireWriteByte(0xBE);	// Issue read scratchpad cmd
+		for (int i = 0; i < 9; i++)
+		{
+			dsScratchPadMem[i] = OneWireMaster.wireReadByte();
+		}
+
+		// check if valid frame
+		if (OneWireMaster.crc8(dsScratchPadMem, 8) == dsScratchPadMem[8])
+		{
+			Mem[1] = dsScratchPadMem[1];	// msb
+			Mem[0] = dsScratchPadMem[0];	// lsb
+			Result = (float)((int16_t)(Mem[1] << 8 | Mem[0]) / 16.0);	// twos complement conversion
+			UserData = (dsScratchPadMem[2] << 8 | dsScratchPadMem[3]);
+		}
+		if (Result > -127) break;	// loop up to 3 times to get valid data
+		delay(1000);
+	}
+	return (Result > -127);
+}
+
+void SetUserDataMaster(byte Addr[], int NewValue)
+{
+	OneWireMaster.wireReset();
+	OneWireMaster.wireSelect(Addr);
+	OneWireMaster.wireWriteByte(0x4E);	// begin write to scratchpad
+
+	OneWireMaster.wireWriteByte(NewValue >> 8);
+	OneWireMaster.wireWriteByte(NewValue & 255);
+	OneWireMaster.wireWriteByte(9);		// set resolution to 0.5C
+	delay(10);
+
+	OneWireMaster.wireReset();
+	OneWireMaster.wireSelect(Addr);
+
+	OneWireMaster.wireWriteByte(0x48);	// copy scratchpad to eeprom
+	delay(100);
+}
+
