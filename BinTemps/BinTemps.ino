a26317847ae3ccf71b0a58affef81cd164ccd29f
SK21 <SK21@users.noreply.github.com>
2026-03-08 15:23:08 -0600

    update module code

--- BinTemps/BinTemps.ino
+++ BinTemps/BinTemps.ino
@@ -1,17 +1,6 @@
 
-// Temperature monitor module
+// Bin temperature module
 // Wemos D1 mini Pro, ESP 12F    board: LOLIN(Wemos) D1 R2 & mini  or NodeMCU 1.0 (ESP-12E Module)
-# define InoDescription "TMmodule   23-Jul-2024"
-
-#define InoID 23074  // change to load default values
-
-// packet description:
-// start,packet type,break,data,break,sensor Rom Code,break
-// packet types:                                                  examples:
-// 0  heartbeat to signal still connected                         ^0|||       heartbeat
-// 1  command sensors to report either 0 or a specific board      ^1||0|      all sensors report
-// 2  set userdata                                                ^2|NewValue|RomCode|     set userdata for sensor
-// 3  get userdata                                                ^3||RomCode|     get userdata for sensor
 
 // user data saved on the sensor:
 // 16 bit data
@@ -30,6 +19,9 @@
 #include <Wire.h>
 #include "DS2482.h" 		    // https://github.com/paeaetech/paeae
 
+# define InoDescription "BinTemps 08-Mar-2026"
+#define InoID 8036  // change to load default values
+
 #define SDApin  4
 #define SCLpin  5
 
@@ -38,19 +30,20 @@ const uint32_t SampleTime = 3600000;    // update temps every hour
 
 struct ModuleData
 {
-    char Name[12] = "Module0";
-    char SSID[32] = "ssid";
-    char Password[32] = "password";
-    IPAddress ServerIP = IPAddress(192,168,1,1);
-    uint16_t Port = 1600;
-    byte Pin1 = 5;  // D1
-    byte Pin2 = 4;  // D2
-    byte Pin3 = 14; // D5
-    byte Pin4 = 12; // D6
-    bool UseDS2482 = false;
-    bool UseWifi = false;
-    bool StrongPullup = false;
-    byte PullupPin = 13;
+	byte ID = 0;
+	char Name[12] = "Module0";
+	char SSID[32] = "ssid";
+	char Password[32] = "password";
+	IPAddress ServerIP = IPAddress(192, 168, 1, 1);
+	uint16_t Port = 1600;
+	byte Pin1 = 5;  // D1
+	byte Pin2 = 4;  // D2
+	byte Pin3 = 14; // D5
+	byte Pin4 = 12; // D6
+	bool UseDS2482 = false;
+	bool UseWifi = false;
+	bool StrongPullup = false;
+	byte PullupPin = 13;
 };
 
 ModuleData MDL;
@@ -62,10 +55,10 @@ DS2482 OneWireMaster(0);
 
 struct SensorData
 {
-    byte ID[8];
-    byte BusID;
-    byte Temperature[2];	// msb [1], lsb [0]
-    byte UserData[2];
+	byte ID[8];
+	byte BusID;
+	byte Temperature[2];	// msb [1], lsb [0]
+	byte UserData[2];
 };
 
 SensorData Sensors[MaxSensors];
@@ -98,217 +91,239 @@ ESP8266WebServer server(80);
 
 uint32_t Readtime;
 
+uint8_t MacAddr[6];
+
 void setup()
 {
-    Serial.begin(38400);
-    delay(5000);
-    Serial.println();
-    Serial.println();
-    Serial.println(InoDescription);
-    Serial.println();
+	Serial.begin(38400);
+	delay(5000);
+	Serial.println();
+	Serial.println();
+	Serial.println(InoDescription);
+	Serial.println();
 
-    EEPROM.begin(512);
-    int16_t StoredID;
-    EEPROM.get(0, StoredID);
-    if (StoredID == InoID)
-    {
-        Serial.println("Loading stored settings.");
-        EEPROM.get(10, MDL);
-    }
-    else
-    {
-        Serial.println("Updating stored settings.");
-        EEPROM.put(0, InoID);
-        EEPROM.put(10, MDL);
-        EEPROM.commit();
-    }
+	EEPROM.begin(512);
+	int16_t StoredID;
+	EEPROM.get(0, StoredID);
+	if (StoredID == InoID)
+	{
+		Serial.println("Loading stored settings.");
+		EEPROM.get(10, MDL);
+	}
+	else
+	{
+		Serial.println("Updating stored settings.");
+		EEPROM.put(0, InoID);
+		EEPROM.put(10, MDL);
+		EEPROM.commit();
+	}
 
-    Serial.print("Module name: ");
-    Serial.println(MDL.Name);
+	Serial.print("Module name: ");
+	Serial.println(MDL.Name);
 
-    StartOTA();
+	StartOTA();
 
-    // start with station mode off
-    WiFi.disconnect(true);
+	// start with station mode off
+	WiFi.disconnect(true);
 
-    String AP = MDL.Name;
-    AP += "  (";
-    AP += WiFi.macAddress();
-    AP += ")";
-    WiFi.softAP(AP);
+	WiFi.macAddress(MacAddr);
+	String AP = MDL.Name;
+	AP += "  (";
+	AP += WiFi.macAddress();
+	AP += ")";
+	WiFi.softAP(AP);
 
-    Serial.print("Access Point name: ");
-    Serial.println(AP);
+	Serial.print("Access Point name: ");
+	Serial.println(AP);
 
-    // web server
-    Serial.println();
-    Serial.println("Starting Web Server");
-    server.on("/", HandleRoot);
-    server.on("/page1", HandleTemps);
-    server.on("/page2", HandlePage2);
-    server.on("/update", DoUpdate);
-    server.onNotFound(HandleRoot);
-    server.begin();
+	// web server
+	Serial.println();
+	Serial.println("Starting Web Server");
+	server.on("/", HandleRoot);
+	server.on("/page1", HandleTemps);
+	server.on("/page2", HandlePage2);
+	server.on("/update", DoUpdate);
+	server.onNotFound(HandleRoot);
+	server.begin();
 
-    // DS2482
-    if (MDL.UseDS2482)
-    {
-        ErrorCount = 0;
-        Serial.println("");
-        Serial.println("Connecting to DS2482 One-Wire Master...");
-        Wire.begin(SDApin, SCLpin);
-        OneWireMaster.reset();
+	// DS2482
+	if (MDL.UseDS2482)
+	{
+		ErrorCount = 0;
+		Serial.println("");
+		Serial.println("Connecting to DS2482 One-Wire Master...");
+		Wire.begin(SDApin, SCLpin);
+		OneWireMaster.reset();
 
-        while (!DS2842Connected)
-        {
-            //configure DS2482 to use active pull-up instead of pull-up resistor 
-            DS2842Connected = OneWireMaster.configure(DS2482_CONFIG_APU);
-            Serial.print(".");
-            delay(500);
-            if (ErrorCount++ > 10) break;
-        }
-        Serial.println("");
-        if (DS2842Connected)
-        {
-            Serial.println("DS2482 found.");
-        }
-        else
-        {
-            Serial.println("DS2482 not found.");
-        }
-    }
-    else
-    {
-        Serial.println("");
-        Serial.println("DS2482 disabled.");
-        Serial.println("Using GPIOs for signals.");
-    }
+		while (!DS2842Connected)
+		{
+			//configure DS2482 to use active pull-up instead of pull-up resistor 
+			DS2842Connected = OneWireMaster.configure(DS2482_CONFIG_APU);
+			Serial.print(".");
+			delay(500);
+			if (ErrorCount++ > 10) break;
+		}
+		Serial.println("");
+		if (DS2842Connected)
+		{
+			Serial.println("DS2482 found.");
+		}
+		else
+		{
+			Serial.println("DS2482 not found.");
+		}
+	}
+	else
+	{
+		Serial.println("");
+		Serial.println("DS2482 disabled.");
+		Serial.println("Using GPIOs for signals.");
+	}
 
-    if (MDL.StrongPullup)
-    {
-        // provide 5V for DS2842 to use for strong pullup
-        pinMode(MDL.PullupPin, OUTPUT);
-        digitalWrite(MDL.PullupPin, HIGH);
-    }
+	if (MDL.StrongPullup)
+	{
+		// provide 5V for DS2842 to use for strong pullup
+		pinMode(MDL.PullupPin, OUTPUT);
+		digitalWrite(MDL.PullupPin, HIGH);
+	}
 
-    Readtime = millis() - SampleTime;
+	Readtime = millis() - SampleTime;
 
-    Serial.println("");
-    Serial.println("Finished Setup");
-    Serial.println("");
+	Serial.println("");
+	Serial.println("Finished Setup");
+	Serial.println("");
 }
 
 void loop()
 {
-    ArduinoOTA.handle();
-    server.handleClient();
+	ArduinoOTA.handle();
+	server.handleClient();
 
-    if ((MDL.UseWifi) && (WiFi.status() == WL_CONNECTED))
-    {
-        CheckTempServer();    //check server connected
-        ReadTempServer();     //check for message from server
-        checkchunks();    //break up and process message
-    }
+	if ((MDL.UseWifi) && (WiFi.status() == WL_CONNECTED))
+	{
+		CheckTempServer();    //check server connected
+		ReadTempServer();     //check for message from server
+		checkchunks();    //break up and process message
+	}
 
 	if (millis() - Readtime > SampleTime)
 	{
 		Readtime = millis();
 
-        if (MDL.UseWifi && (WiFi.status() != WL_CONNECTED))
-        {
-            ConnectWifi();
-            CheckTempServer();
-        }
+		if (MDL.UseWifi && (WiFi.status() != WL_CONNECTED))
+		{
+			ConnectWifi();
+			CheckTempServer();
+		}
 
-        UpdateTmps();
+		UpdateTmps();
 
-        int Min = SampleTime / 60000;
-        Serial.print("");
-        Serial.print("Updating in ");
-        Serial.print(Min);
-        Serial.println(" minutes.");
+		int Min = SampleTime / 60000;
+		Serial.print("");
+		Serial.print("Updating in ");
+		Serial.print(Min);
+		Serial.println(" minutes.");
 	}
 }
 
+bool GoodCRC(byte Data[], byte Length)
+{
+	byte ck = CRC(Data, Length - 1, 0);
+	bool Result = (ck == Data[Length - 1]);
+	return Result;
+}
+
+byte CRC(byte Chk[], byte Length, byte Start)
+{
+	byte Result = 0;
+	for (int i = Start; i < Length; i++)
+	{
+		Result += Chk[i];
+	}
+	return Result;
+}
+
+
+
 void UpdateTmps()
 {
-    if (DS2842Connected)
-    {
-        UpdateSensorsMaster();
-    }
-    else
-    {
-        UpdateSensors();
-    }
+	if (DS2842Connected)
+	{
+		UpdateSensorsMaster();
+	}
+	else
+	{
+		UpdateSensors();
+	}
 }
 
 String IPtoString(IPAddress addr)
 {
-    String ans = String(addr[0]) + "." + String(addr[1]) + "." + String(addr[2]) + "." + String(addr[3]);
-    return ans;
+	String ans = String(addr[0]) + "." + String(addr[1]) + "." + String(addr[2]) + "." + String(addr[3]);
+	return ans;
 }
 
 String addressTostring(byte addr[8])    //converts 8 byte address to hex and places a space between bytes
 {
-    String ans = "";
-    for (byte i = 0; i < 8; i++)
-    {
-        ans = ans + DecToHex(addr[i]);
-        ans = ans + " ";
-    }
-    return ans;
+	String ans = "";
+	for (byte i = 0; i < 8; i++)
+	{
+		ans = ans + DecToHex(addr[i]);
+		ans = ans + " ";
+	}
+	return ans;
 }
 
 void stringToaddress(String ad)   //converts 8 hex spaced address string to 8 byte decimal
 {
-    int bcount = 0;
-    int len = ad.length() + 1;
-    char prt[len];
-    ad.toCharArray(prt, len);      // convert string to character array
-    String v = "";
-    for (int p = 0; p < len; p++)   // go through array to find hex address parts between spaces
-    {
-        if (prt[p] == ' ')          // check for space delimiter
-        {
-            RomCode[bcount] = hexToDec(v); //convert string hex address part to byte
-            v = "";
-            bcount = bcount + 1;
-            if (bcount == 8)
-            {
-                return; //done
-            }
-        }
-        else
-        {
-            v = v + String(prt[p]); //build string hex address part
-        }
-    }
-    RomCode[bcount] = hexToDec(v);  // needed if there is no last space
+	int bcount = 0;
+	int len = ad.length() + 1;
+	char prt[len];
+	ad.toCharArray(prt, len);      // convert string to character array
+	String v = "";
+	for (int p = 0; p < len; p++)   // go through array to find hex address parts between spaces
+	{
+		if (prt[p] == ' ')          // check for space delimiter
+		{
+			RomCode[bcount] = hexToDec(v); //convert string hex address part to byte
+			v = "";
+			bcount = bcount + 1;
+			if (bcount == 8)
+			{
+				return; //done
+			}
+		}
+		else
+		{
+			v = v + String(prt[p]); //build string hex address part
+		}
+	}
+	RomCode[bcount] = hexToDec(v);  // needed if there is no last space
 }
 
 byte hexToDec(String hexString)
 {
-    byte decValue = 0;
-    int nextInt;
-    for (int i = 0; i < hexString.length(); i++)
-    {   // only allow 2 character hex address parts    
-        nextInt = int(hexString.charAt(i));
-        if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
-        if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
-        if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
-        nextInt = constrain(nextInt, 0, 15);
-        decValue = (decValue * 16) + nextInt;
-    }
-    return decValue;
+	byte decValue = 0;
+	int nextInt;
+	for (int i = 0; i < hexString.length(); i++)
+	{   // only allow 2 character hex address parts    
+		nextInt = int(hexString.charAt(i));
+		if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+		if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+		if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+		nextInt = constrain(nextInt, 0, 15);
+		decValue = (decValue * 16) + nextInt;
+	}
+	return decValue;
 }
 
 String DecToHex(byte dec)
 {
-    String ans = String(dec, HEX);
-    ans.toUpperCase();
-    if (ans.length() == 1)
-    {
-        ans = "0" + ans;
-    }
-    return ans;
+	String ans = String(dec, HEX);
+	ans.toUpperCase();
+	if (ans.length() == 1)
+	{
+		ans = "0" + ans;
+	}
+	return ans;
 }
--- BinTemps/Comm.ino
+++ BinTemps/Comm.ino
@@ -77,6 +77,169 @@ void CheckTempServer()
 
 void ReadTempServer()
 {
+	const uint8_t MaxBuffer = 50;	// bytes
+	uint16_t len = client.available();
+	if (len)
+	{
+		byte data[MaxBuffer];
+		client.read(data, MaxBuffer);
+		ReadPGNs(data, len);
+	}
+}
+
+void ReadPGNs(byte data[], uint16_t len)
+{
+	uint16_t PGN = data[1] << 8 | data[0];
+	byte PGNlength;
+	switch (PGN)
+	{
+	case 30820:
+		// heartbeat
+		// 0	header lo	100
+		// 1	header hi	120
+		// 2	crc
+
+		PGNlength = 3;
+		if (len > PGNlength - 1)
+		{
+			if (GoodCRC(data, PGNlength))
+			{
+				pulsetime = millis();
+			}
+		}
+		break;
+
+	case 30821:
+		// command
+		// 0	header lo	101
+		// 1	header hi	120
+		// 2	module ID
+		// 3	command
+		//		- bit 0	send sensor temps
+		//		- bit 1	send module description
+		//		- bit 2 send sensor serial # and user data
+		// 4	crc
+
+		PGNlength = 5;
+		if (len > PGNlength - 1)
+		{
+			if (GoodCRC(data, PGNlength))
+			{
+				byte InCommand = data[3];
+				if ((InCommand & 1) == 1) SendTemps();
+
+				if (data[2] == MDL.ID)
+				{
+					if ((InCommand & 2) == 2) SendModuleDescription();
+					if ((InCommand & 4) == 4) SendSensorDescription();
+				}
+			}
+		}
+		break;
+
+	case 30822:
+		// set module description
+		// 0	header lo	102
+		// 1	header hi	120
+		// 2-7	module mac
+		// 8	new module ID
+		// 9-18	new module name
+		// 19	crc
+
+		PGNlength = 20;
+		if (len > PGNlength - 1)
+		{
+			if (GoodCRC(data, PGNlength))
+			{
+
+			}
+		}
+		break;
+
+	case 30823:
+		// set sensor description
+		// 0	header lo	103
+		// 1	header hi	120
+		// 2	module ID
+		// 3-10	sensor serial #
+		// 11	user data, byte 0
+		// 12	user data, byte 1
+		// 13	CRC
+
+		PGNlength = 14;
+		if (len > PGNlength - 1)
+		{
+			if (GoodCRC(data, PGNlength))
+			{
+
+			}
+		}
+		break;
+	}
+}
+
+void SendTemps()
+{
+	// temperatures
+	// 0	header lo	110
+	// 1	header hi	120
+	// 2	module ID
+	// 3-10	sensor serial #
+	// 11	temp lo
+	// 12	temp hi
+	// 13	Remaining sensors 
+	// 14	CRC
+
+	byte PGNlength = 15;
+	byte data[PGNlength];
+
+	for (int i = 0; i < SensorCount; i++)
+	{
+		memset(data, 0, PGNlength);
+		data[0] = 110;
+		data[1] = 120;
+		data[2] = MDL.ID;
+
+		memcpy(&data[3], Sensors[i].ID, 8);
+
+		data[11] = Sensors[i].Temperature[0];
+		data[12] = Sensors[i].Temperature[1];
+		data[13] = SensorCount - i - 1;
+		data[14] = CRC(data, PGNlength, 0);
+		client.write(data, PGNlength);
+	}
+}
+
+void SendModuleDescription()
+{
+	// module description
+	// 0	header lo	111
+	// 1	header hi	120
+	// 2	module ID
+	// 3-8	module mac
+	// 9-18	module name
+	// 19	CRC
+
+	byte PGNlength = 20;
+	byte data[PGNlength];
+	data[0] = 111;
+	data[1] = 120;
+	data[2] = MDL.ID;
+	memcpy(&data[3], MacAddr, 6);
+	memcpy(&data[9], MDL.Name, 10);
+	data[19] = CRC(data, PGNlength, 0);
+	client.write(data, PGNlength);
+}
+
+void SendSensorDescription()
+{
+
+}
+
+
+
+void ReadTempServerOld()
+{
 	int Count = 0;
 	while (client.available())
 	{
